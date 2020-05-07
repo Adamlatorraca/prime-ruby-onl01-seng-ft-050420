df46a35c6eea1705138f2ebea24ecbf7ca143f80
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 21:40:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 # Add  code here!
 def prime?(integer)
-  if integer % 2 == 0
-    return true
-  else
-    return false
-end
+  n = 2
+  while n < integer
+      return false if num % n == 0
+      n += 1
+  end
+    true
 end
